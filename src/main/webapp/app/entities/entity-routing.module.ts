591890a143ae854bb7178a24de9449a661d35efa
Madhurendra Singh <msingh356@gmail.com>
2021-05-23 12:14:36 +0530

    JDL test

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,46 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        data: { pageTitle: 'jhipsterTest2App.region.home.title' },
+        loadChildren: () => import('./region/region.module').then(m => m.RegionModule),
+      },
+      {
+        path: 'country',
+        data: { pageTitle: 'jhipsterTest2App.country.home.title' },
+        loadChildren: () => import('./country/country.module').then(m => m.CountryModule),
+      },
+      {
+        path: 'location',
+        data: { pageTitle: 'jhipsterTest2App.location.home.title' },
+        loadChildren: () => import('./location/location.module').then(m => m.LocationModule),
+      },
+      {
+        path: 'department',
+        data: { pageTitle: 'jhipsterTest2App.department.home.title' },
+        loadChildren: () => import('./department/department.module').then(m => m.DepartmentModule),
+      },
+      {
+        path: 'task',
+        data: { pageTitle: 'jhipsterTest2App.task.home.title' },
+        loadChildren: () => import('./task/task.module').then(m => m.TaskModule),
+      },
+      {
+        path: 'employee',
+        data: { pageTitle: 'jhipsterTest2App.employee.home.title' },
+        loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+      },
+      {
+        path: 'job',
+        data: { pageTitle: 'jhipsterTest2App.job.home.title' },
+        loadChildren: () => import('./job/job.module').then(m => m.JobModule),
+      },
+      {
+        path: 'job-history',
+        data: { pageTitle: 'jhipsterTest2App.jobHistory.home.title' },
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.JobHistoryModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
